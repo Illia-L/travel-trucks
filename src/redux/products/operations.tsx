a5cf2e2af8e927929232adb12674e293e4610cd7
Illia Liashko <2275040@gmail.com>
2025-08-20 23:14:56 +0300

    fix types

--- src/redux/products/operations.tsx
+++ src/redux/products/operations.tsx
@@ -1,33 +1,32 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { fetchAllProducts } from '../../utils/api';
+import { fetchAllProducts, type AllProductsData } from '../../utils/api';
 import { clearProducts } from './slice';
 import type { RootState } from '../store';
-import type { AxiosError } from 'axios';
 
-export const loadProducts = createAsyncThunk<void, void, { state: RootState }>(
-  'pruducts/load',
-  async (_, { rejectWithValue, dispatch, getState }) => {
-    const state = getState();
-    const filter = { ...state.products.filter };
-    const { page, perPage } = filter;
+export const loadProducts = createAsyncThunk<
+  AllProductsData,
+  void,
+  { state: RootState }
+>('pruducts/load', async (_, { rejectWithValue, dispatch, getState }) => {
+  const state = getState();
+  const filter = { ...state.products.filter };
+  const { page } = filter;
+  const { perPage, ...queryParams } = filter;
 
-    delete filter.perPage;
+  if (page === 1) dispatch(clearProducts());
 
-    if (page === 1) dispatch(clearProducts());
+  try {
+    const data = await fetchAllProducts(queryParams);
 
-    try {
-      const data = await fetchAllProducts(filter);
+    const paginatedItems = data.items.slice(
+      (page - 1) * perPage,
+      (page - 1) * perPage + perPage
+    );
 
-      const paginatedItems = data.items.slice(
-        (page - 1) * perPage,
-        (page - 1) * perPage + perPage
-      );
+    data.items = paginatedItems;
 
-      data.items = paginatedItems;
-
-      return data;
-    } catch {
-      return rejectWithValue('');
-    }
+    return data;
+  } catch {
+    return rejectWithValue('');
   }
-);
+});
